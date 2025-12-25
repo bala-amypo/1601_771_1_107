f15b430dee9db7da34947fcfeadfe81dbc260613
bala-amypo <bala@amypo.in>
2025-12-25 16:08:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/DamageClaimRepository.java
+++ src/main/java/com/example/demo/repository/DamageClaimRepository.java
@@ -3,9 +3,13 @@ package com.example.demo.repository;
 import com.example.demo.model.DamageClaim;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DamageClaimRepository extends JpaRepository<DamageClaim, Long> {
 
     Optional<DamageClaim> findByClaimNumber(String claimNumber);
+
+    // 🔥 REQUIRED BY MasterTestNGSuiteTest
+    List<DamageClaim> findByParcel_Id(Long parcelId);
 }
