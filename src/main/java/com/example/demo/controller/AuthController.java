7cbcb997ac730a1113d22ff73224cf3f188d2436
bala-amypo <bala@amypo.in>
2025-12-29 14:28:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,32 +1,3 @@
-// package com.example.demo.controller;
-
-// import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-// import com.example.demo.model.User;
-// import com.example.demo.service.UserService;
-// import org.springframework.web.bind.annotation.*;
-
-
-// @RestController
-// @SecurityRequirement(name = "bearerAuth")
-// @RequestMapping("/users")
-// public class AuthController {
-
-//     private final UserService service;
-
-//     public AuthController(UserService service) {
-//         this.service = service;
-//     }
-
-//     @PostMapping("/register")
-//     public User register(@RequestBody User user) {
-//         return service.register(user);
-//     }
-    
-//     @GetMapping("/{email}")
-//     public User getByEmail(@PathVariable String email) {
-//         return service.findByEmail(email);
-//     }
-// }
 package com.example.demo.controller;
 
 import com.example.demo.dto.AuthRequest;
